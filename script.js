150cc1a345320aa3fdd0ed41b017d0503cf35df1
Kyle Croxall <kylecroxall@gmail.com>
2021-07-18 02:23:22 +0100

    Changed quote fetcher to use async/await

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-require('dotenv').config();
-const apiKey = process.env.API_KEY;
+const apiKey = 'c3peufaad3ifkq8grbs0';
 
-function fetchLatestQuote (ticker) {
-    fetch(`https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${apiKey}`)
-    .then(response => response.json())
-    .then(tickerData =>
-        console.log(tickerData)
-    )
-    .catch(error => console.log('The server has returned with error: ' + error))
+async function fetchLatestQuote (ticker) {
+    const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${apiKey}`
+    const response = await fetch(url);
+    const tickerData = await response.json();
+    const formattedQuote = {
+        tickerSymbol: ticker,
+        openPrice: tickerData.o,
+        highPrice: tickerData.h,
+        lowPrice: tickerData.l,
+        currentPrice: tickerData.c,
+        previousClose: tickerData.pc,
+    }
+    console.log(formattedQuote)
+
+
+   
+
 }
 
-fetchLatestQuote('GME');
+fetchLatestQuote('GME')
+fetchLatestQuote('AMC')
+fetchLatestQuote('QQQ')
+fetchLatestQuote('SPY')
+fetchLatestQuote('BB')
+fetchLatestQuote('SPCE')
+fetchLatestQuote('TSLA')
+console.log("Hi");
+
+
+
+// console.log("Here is the object: " + myQuote)
